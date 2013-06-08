a8976437bdb9f16972d6aec6ac38a9bbf2104415
Tsss-Pc1 <Tsss-Pc1@Tsss-Pc1-PC>
2013-06-08 11:27:24 +0530

    test1

---
+++ 5172013/testCases/src/test/java/com/dealwallet/webdriver/selenium/Samles/SampleTest1.java
@@ -0,0 +1,13 @@
+package com.dealwallet.webdriver.selenium.Samles;
+
+public class SampleTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
