a942205059fcbf9b02511f3e27be565eb685caca
gvs048 <Tsss-Pc1@Tsss-Pc1-PC>
2013-06-25 09:59:44 +0530

    updates

---
+++ 5172013/testCases/src/test/java/com/dealwallet/webdriver/samples/CustomException.java
@@ -0,0 +1,13 @@
+package com.dealwallet.webdriver.samples;
+
+public class CustomException {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
